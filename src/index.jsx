e621fe96eba791a7e8c25de898c38c36386e7a16
ThibaultClerc <thibault.clerc2@gmail.com>
2020-10-20 11:13:31 +0200

    app ready

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends React.Component {
+  render () {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
